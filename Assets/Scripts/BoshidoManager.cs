8390548cbb07b3afc334c785cbcc404339b7f39e
Danny Hawk <HawkDanny@users.noreply.github.com>
2025-04-12 14:57:11 -0400

    build for bday

--- Assets/Scripts/BoshidoManager.cs
+++ Assets/Scripts/BoshidoManager.cs
@@ -21,10 +21,24 @@ public class BoshidoManager : MonoBehaviour
     public Transform yellowYoshiModel;
     private Sequence currentSequence;
 
-    [Header("Streak Settings")]
+    [Header("Highest Streak")]
+    public TextMeshProUGUI streakUI;
+    private int highestStreak = 0;
+
+    [Header("Effects Settings")]
     public float animStartLength;
     public float animSpeedupFactor;
     private float currentAnimLength;
+    public Transform bigBoshi;
+    public Transform bigYellowYoshi;
+    public GameObject boshiWinPointUI;
+    public GameObject yellowYoshiWinPointUI;
+    public float bigAppearanceLength;
+    public float bigPunchPower;
+
+    [Header("Videos")]
+    public List<GameObject> videos = new List<GameObject>();
+    private int videoIndex = 0;
 
     //scores
     private int boshiScore;
@@ -53,7 +67,7 @@ public class BoshidoManager : MonoBehaviour
             if (boshiWins) Score("boshi");
             if (yellowYoshiWins) Score("yellow yoshi");
 
-            Invoke("Reload", 14);
+            Invoke("Reload", 3.5f);
         }
     }
 
@@ -81,6 +95,8 @@ public class BoshidoManager : MonoBehaviour
             if (boshiScore == 0) ZeroScore("yellow yoshi");
             score = ++boshiScore;
             dir = -1;
+
+            StartCoroutine("BoshiWinPoint");
         }
         else if (who == "yellow yoshi")
         {
@@ -90,6 +106,8 @@ public class BoshidoManager : MonoBehaviour
             //if this score is coming from zero, they broke the other player's streak
             if (yellowYoshiScore == 0) ZeroScore("boshi");
             score = ++yellowYoshiScore;
+
+            StartCoroutine("YellowYoshiWinPoint");
         }
         else Debug.LogError("Typo when calling Score");
 
@@ -97,19 +115,59 @@ public class BoshidoManager : MonoBehaviour
         scoreUI.gameObject.SetActive(true);
         scoreUI.text = score.ToString();
 
+        //update high score if relevant
+        if (score > highestStreak)
+        {
+            streakUI.text = "best - " + who + " with " + score;
+            highestStreak = score;
+        }
+
         //speed up animation
         currentAnimLength *= 1/animSpeedupFactor;
         Sequence currentSequence = DOTween.Sequence();
+        currentSequence.stringId = "spinning";
         currentSequence.SetLoops(-1);
         currentSequence.Append(model.DOLocalRotate(new Vector3(0, 360 * dir, 0), currentAnimLength, RotateMode.LocalAxisAdd).SetEase(Ease.Linear));
         currentSequence.Play();
 
-        switch (score)
+
+        //change video
+        if (score == 1)
         {
-            case int n when n == 1:
-                
-                break;
+            videos[videoIndex].SetActive(false);
+            videoIndex = 0;
         }
+        else
+        {
+            if (score % 5 == 0)
+            {
+                videos[videoIndex].SetActive(false);
+                videoIndex = ++videoIndex % videos.Count;
+                videos[videoIndex].SetActive(true);
+            }
+        }
+    }
+
+    private IEnumerator BoshiWinPoint()
+    {
+        yield return new WaitForSeconds(0.5f);
+        bigBoshi.gameObject.SetActive(true);
+        boshiWinPointUI.SetActive(true);
+        bigBoshi.DOPunchPosition(new Vector3(1, 1, 1) * bigPunchPower, bigAppearanceLength, 50);
+        yield return new WaitForSeconds(bigAppearanceLength);
+        bigBoshi.gameObject.SetActive(false);
+        boshiWinPointUI.SetActive(false);
+    }
+
+    private IEnumerator YellowYoshiWinPoint()
+    {
+        yield return new WaitForSeconds(0.5f);
+        bigYellowYoshi.gameObject.SetActive(true);
+        yellowYoshiWinPointUI.SetActive(true);
+        bigYellowYoshi.DOPunchPosition(new Vector3(1, 1, 1) * bigPunchPower, bigAppearanceLength, 50);
+        yield return new WaitForSeconds(bigAppearanceLength);
+        bigYellowYoshi.gameObject.SetActive(false);
+        yellowYoshiWinPointUI.SetActive(false);
     }
 
     private void ZeroScore(string who)
@@ -121,13 +179,13 @@ public class BoshidoManager : MonoBehaviour
         {
             yellowYoshiScoreUI.gameObject.SetActive(false);
             yellowYoshiScore = 0;
-            DOTween.PauseAll();
+            DOTween.Pause("spinning");
         }
         else if (who == "boshi")
         {
             boshiScoreUI.gameObject.SetActive(false);
             boshiScore = 0;
-            DOTween.PauseAll();
+            DOTween.Pause("spinning");
         }
     }
 
